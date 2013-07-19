bedb30b0b7167eca1e38a5ecb7f91dc2897e98fd
Matteo Gerola <mgerola@create-net.org>
2013-07-19 12:09:34 -0700

    Add first Network implementation

--- src/net/onrc/openvirtex/elements/network/PhysicalNetwork.java
+++ src/net/onrc/openvirtex/elements/network/PhysicalNetwork.java
@@ -3,10 +3,53 @@
  */
 package net.onrc.openvirtex.elements.network;
 
+import java.util.HashSet;
+
+import net.onrc.openvirtex.elements.datapath.PhysicalSwitch;
+import net.onrc.openvirtex.elements.link.PhysicalLink;
+import net.onrc.openvirtex.elements.port.PhysicalPort;
+
 /**
  * @author gerola
- *
+ * 
  */
-public class PhysicalNetwork extends Network {
+
+public class PhysicalNetwork extends
+        Network<PhysicalSwitch, PhysicalPort, PhysicalLink> {
+
+    private HashSet<Uplink> uplinkList;
+
+    public HashSet<Uplink> getUplinkList() {
+	return this.uplinkList;
+    }
+
+    public void setUplinkList(final HashSet<Uplink> uplinkList) {
+	this.uplinkList = uplinkList;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * net.onrc.openvirtex.elements.network.Network#sendLLDP(java.lang.Object)
+     */
+    @Override
+    public void sendLLDP(final PhysicalSwitch sw) {
+	// TODO Auto-generated method stub
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * net.onrc.openvirtex.elements.network.Network#receiveLLDP(java.lang.Object
+     * )
+     */
+    @Override
+    public void receiveLLDP(final PhysicalSwitch sw) {
+	// TODO Auto-generated method stub
+
+    }
 
 }
