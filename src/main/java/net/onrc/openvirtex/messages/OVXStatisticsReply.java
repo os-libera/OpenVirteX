d212b671e52274601f8dbbf172a910eb1436438b
alshabib <alshabibi.ali@gmail.com>
2014-04-08 09:58:05 -0700

    Changed the license to Apache 2.0
    removed GPL license file and added apache 2 license file
    
    Change-Id: I9cfc7d7b9b9bf59ae1ab96264c8f5dff121100bb

--- src/main/java/net/onrc/openvirtex/messages/OVXStatisticsReply.java
+++ src/main/java/net/onrc/openvirtex/messages/OVXStatisticsReply.java
@@ -1,11 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2013 Open Networking Laboratory.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Copyright 2014 Open Networking Laboratory
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
-
 package net.onrc.openvirtex.messages;
 
 import net.onrc.openvirtex.elements.datapath.PhysicalSwitch;
