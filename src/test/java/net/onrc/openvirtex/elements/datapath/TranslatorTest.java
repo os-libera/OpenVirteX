d475e68913afea22b8676bff5ead9df42ac14723
fahad <fahad@onlab.us>
2014-06-11 11:19:47 -0700

    TestCommit (For Jenkins) Add PhysicalSwitch JUnit Test (OVX-346) -Removed tabs -Added Comments. -Corrected indentation. -Added DummyPhysicalSwitch thats created all the dummy features required for the tests.  -Testing of PhysicalSwitch class has been completed.
    Change-Id: I8ff75af8bddb0855287dd6ac25623f7623190545

--- src/test/java/net/onrc/openvirtex/elements/datapath/TranslatorTest.java
+++ src/test/java/net/onrc/openvirtex/elements/datapath/TranslatorTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,7 +26,7 @@ import org.openflow.protocol.OFHello;
 
 public class TranslatorTest extends TestCase {
 
-    OpenVirteXController ctl = null;
+    OpenVirteXController             ctl = null;
     private XidTranslator<OVXSwitch> translator;
 
     public TranslatorTest(final String name) {
@@ -64,7 +64,7 @@ public class TranslatorTest extends TestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        ctl = new OpenVirteXController(new CmdLineSettings());
+        this.ctl = new OpenVirteXController(new CmdLineSettings());
         this.translator = new XidTranslator<OVXSwitch>();
     }
 
