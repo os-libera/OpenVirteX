22dc9a91e1f310e78b777bd33b41840d27cc3000
Aryan TaheriMonfared <aryantaheri@gmail.com>
2015-02-03 01:13:54 +0100

    TCP port mapping resource allocation

--- src/main/java/net/onrc/openvirtex/routing/nat/NatPortManager.java
+++ src/main/java/net/onrc/openvirtex/routing/nat/NatPortManager.java
@@ -4,7 +4,10 @@ import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicReference;
 
 import net.onrc.openvirtex.elements.address.OVXIPAddress;
-import net.onrc.openvirtex.messages.OVXPacketIn;
+import net.onrc.openvirtex.exceptions.DuplicateIndexException;
+import net.onrc.openvirtex.exceptions.IndexOutOfBoundException;
+import net.onrc.openvirtex.util.BitSetIndex;
+import net.onrc.openvirtex.util.BitSetIndex.IndexType;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,15 +17,14 @@ public class NatPortManager {
 
     private static Logger log = LogManager.getLogger(NatPortManager.class.getName());
     private static AtomicReference<NatPortManager> managerInstance = new AtomicReference<>();
-
+    private BitSetIndex natSrcPortIndex;
     // U16 do 16bit unsigned operation
-    private TreeMap<Short, OVXIPAddress> portMap;
-    protected LRULinkedHashMap<Short, NatTuple> portCache;
+    private LRULinkedHashMap<NatTuple, Short> portCache;
 
 
     public NatPortManager() {
-        portMap = new TreeMap<Short, OVXIPAddress>();
         portCache = new LRULinkedHashMap<>(65535-1024);
+        natSrcPortIndex = new BitSetIndex(IndexType.TCP_NAT);
     }
 
     public static NatPortManager getInstance() {
@@ -35,8 +37,25 @@ public class NatPortManager {
      * @param srcIP
      * @param srcPort
      * @return
+     * @throws IndexOutOfBoundException
      */
-//    public synchronized Short allocatePort(OVXIPAddress srcAddress, Short srcPort) {
-//        portCache.put(key, new NatTuple(srcAddress, srcPort));
-//    }
+    public synchronized Short allocatePort(OVXIPAddress srcAddress, Short srcPort) {
+        NatTuple natTuple = new NatTuple(srcAddress, srcPort);
+        Short natSrcPort = portCache.get(natTuple);
+        if (natSrcPort == null){
+            try {
+                natSrcPort = getNextNatSrcPort();
+            } catch (IndexOutOfBoundException e) {
+                log.debug(e);
+                // TODO: Cache should be cleaned now and a LRU natSrcPort being reused...
+                // + deleting entries
+            }
+            portCache.put(natTuple, natSrcPort);
+        }
+        return natSrcPort;
+    }
+
+    private Short getNextNatSrcPort() throws IndexOutOfBoundException{
+        return (short) (natSrcPortIndex.getNewIndex().shortValue() + 1023);
+    }
 }
